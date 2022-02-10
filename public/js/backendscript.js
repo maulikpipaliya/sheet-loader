de767a2bf9c16666898bb02f0d0e368df204cda6
AlayKharadi <62132315+AlayKharadi@users.noreply.github.com>
2022-02-10 11:27:16 +0530

    excel to json using web-worker

---
+++ public/js/backendscript.js
@@ -0,0 +1,14 @@
+//for web-worker
+let worker = new Worker('/js/web-worker.js');
+let inputs = document.getElementsByClassName("input-file");
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("change", () => {
+        worker.postMessage(inputs[i].files[0]);
+    });
+}
+
+
+worker.onmessage = (event) => {
+    console.log(event.data);
+}
